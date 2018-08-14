70db66da7a405660b0118a0c460f3f6d42b6d847
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-08-14 14:50:03 -0500

    Export snapshot() helper

---
+++ index.js
@@ -0,0 +1,12 @@
+// FIXME: implement blockOut, e.g. take a list of selectors, retrieve their coordinates, and remap
+
+function snapshot(t, label) {
+  const suffix = process.env.SNAPSHOT || process.argv.slice(2).indexOf('--snapshot') !== -1 ? 'base' : 'actual';
+  const imagePath = `${(label || t.testRun.test.name).replace(/\//g, '__or__').replace(/\W+/g, '_')}_${suffix}.png`;
+
+  return t.takeScreenshot(imagePath);
+}
+
+module.exports = {
+  snaphost,
+};
