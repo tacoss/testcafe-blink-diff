461b0b998ded497d58e8cbf6556f6d26b2873e51
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-08-14 16:02:27 -0500

    Fix suffix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // FIXME: implement blockOut, e.g. take a list of selectors, retrieve their coordinates, and remap
 
 function snapshot(t, label) {
-  const suffix = process.env.SNAPSHOT || process.argv.slice(2).indexOf('--snapshot') !== -1 ? 'base' : 'actual';
+  const suffix = (process.env.SNAPSHOT || process.argv.slice(2).indexOf('--snapshot') !== -1) ? 'base' : 'actual';
   const imagePath = `${(label || t.testRun.test.name).replace(/\//g, '__or__').replace(/\W+/g, '_')}_${suffix}.png`;
 
   return t.takeScreenshot(imagePath);
