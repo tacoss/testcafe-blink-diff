dcf345afdbbae9c2ccb18ac620d3b43e5c48bc0c
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-08-14 18:34:36 -0500

    Rename exported helper

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const takeSnapshot = process.argv.slice(2).indexOf('--snapshot');
-const type = (process.env.SNAPSHOT || takeSnapshot !== -1) ? 'base' : 'actual';
+const isTakeSnapshot = process.argv.slice(2).indexOf('--take-snapshot');
+const type = (process.env.SNAPSHOT || isTakeSnapshot !== -1) ? 'base' : 'actual';
 
 function normalize(value) {
   return value.replace(/\W+/g, '_');
 }
 
 // FIXME: implement blockOut, e.g. take a list of selectors, retrieve their coordinates, and remap
-function snapshot(t, label, timeout) {
+function takeSnapshot(t, label, timeout) {
   const filename = normalize(label || t.testRun.test.name).replace(/\//g, '__or__');
   const imagePath = `${normalize(t.testRun.test.fixture.name)}/${filename}/${type}.png`;
 
@@ -14,5 +14,5 @@ function snapshot(t, label, timeout) {
 }
 
 module.exports = {
-  snapshot,
+  takeSnapshot,
 };
