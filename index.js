2e99ed797b05e293e34d69698feb632a45b8992c
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-08-15 17:44:27 -0500

    Prefix tests to keep order; fix

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const GROUP = {};
+
 const isTakeSnapshot = process.argv.slice(2).indexOf('--take-snapshot');
 const snapshotName = isTakeSnapshot > -1 ? process.argv.slice(2)[isTakeSnapshot + 1] : null;
 const type = snapshotName || ((process.env.SNAPSHOT || isTakeSnapshot !== -1) ? 'base' : 'actual');
@@ -10,10 +12,23 @@ function normalize(value) {
     .replace(/\//g, '-or-');
 }
 
+function padLeft(value) {
+  return `000${value}`.substr(-3);
+}
+
 // FIXME: implement blockOut, e.g. take a list of selectors, retrieve their coordinates, remap, etc.
 function takeSnapshot(t, label, timeout) {
+  const groupId = t.testRun.test.fixture.name;
+
+  if (!GROUP[groupId]) {
+    GROUP[groupId] = 0;
+  }
+
+  GROUP[groupId] += 1;
+
+  const offset = padLeft(GROUP[groupId]);
   const filename = normalize(label || t.testRun.test.name);
-  const imagePath = `${normalize(t.testRun.test.fixture.name)}/${filename}/${type}.png`;
+  const imagePath = `${normalize(groupId)}/${offset}_${filename}/${type}.png`;
 
   return t.wait(timeout === false ? 0 : (timeout || 500)).takeScreenshot(imagePath);
 }
